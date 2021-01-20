c885e213a0d9a4f838585e2c89f5bce9cf005980
denyskalinichenko <denis.kalinichenko96@gmail.com>
2021-01-20 21:28:56 +0100

    dialogs

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,15 +4,17 @@ import MyPosts from './MyPosts/MyPosts';
 import s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className={s.content}>
+    return (
     <div>
-      <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
+      <div>
+        <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
+      </div>
+      <div>
+        avatar + description
+      </div>
+      <MyPosts />
     </div>
-    <div>
-      avatar + description
-    </div>
-    <MyPosts />
-    </div>
+    )
 }
 
 export default Profile;
