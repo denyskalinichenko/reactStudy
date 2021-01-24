97ae62f4f2f1e76f3fce339afd7a3ac241c3e3e1
denyskalinichenko <denis.kalinichenko96@gmail.com>
2021-01-24 13:02:21 +0100

    dialogsMessages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import Music from './components/Music/Music';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
 import {BrowserRouter, Route} from "react-router-dom";
 
 
@@ -11,16 +14,19 @@ import {BrowserRouter, Route} from "react-router-dom";
 
 const App = (props) => {
   return (
-    <BrowserRouter>
+  <BrowserRouter>
     <div className='app-wrapper'>
       <Header />
       <Navbar />
       <div className = "app-wrapper-content">
-        <Route path='/dialogs' component = {Dialogs}/>
-        <Route path='/profile' component = {Profile} />
+        <Route  path='/settings' component = {Settings}/>
+        <Route  path='/dialogs'  component = {Dialogs} />
+        <Route  path='/profile'  component = {Profile} />
+        <Route  path='/music'    component = {Music}   />
+        <Route  path='/news'     component = {News}    />
       </div>
     </div>
-    </BrowserRouter>)
+  </BrowserRouter>)
     
 }
 
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs= (props)=> {
     return(
-        <div>
-            Dialogs
+        <div className={s.dialogsMessages}>
+            <div className = {s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to="/dialogs/1">Ola</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/2">Denis</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/3">Alina</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/4">Eba</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/5">Junna</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/6">Misha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/7">Lera</NavLink>
+                </div>
+           </div>
+            <div className={s.messageItems}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>Cześć</div>
+           </div>
         </div>
 
     )
